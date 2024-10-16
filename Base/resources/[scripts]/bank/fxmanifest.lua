eb0909ab30635317e8bcf7644911f35193a6399f
Lucas Hen <oisouhen@gmail.com>
2024-10-16 18:34:02 -0300

    Update fxmanifest.lua

--- Base/resources/[scripts]/bank/fxmanifest.lua
+++ Base/resources/[scripts]/bank/fxmanifest.lua
@@ -5,12 +5,13 @@ lua54 "yes"
 ui_page "web/index.html"
 
 client_scripts {
+	"@vrp/config/Native.lua",
+	"@PolyZone/client.lua",
 	"@vrp/lib/Utils.lua",
 	"client/*"
 }
 
 server_scripts {
-	"@vrp/config/License.lua",
 	"@vrp/lib/Utils.lua",
 	"server/*"
 }
@@ -18,4 +19,11 @@ server_scripts {
 files {
 	"web/*",
 	"web/**/*"
+}
+
+shared_scripts {
+	"@vrp/config/Item.lua",
+	"@vrp/config/Vehicle.lua",
+	"@vrp/config/Global.lua",
+	"@vrp/config/Drops.lua"
 }
