cda473e24491e31203200301c10dda9fb527a39b
Lucas Hen <oisouhen@gmail.com>
2024-10-17 14:51:41 -0300

    Update core.lua

--- Base/resources/[scripts]/trucker/server/core.lua
+++ Base/resources/[scripts]/trucker/server/core.lua
@@ -38,22 +38,6 @@ function Hensa.CheckDelay()
 	end
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
--- CHECKWORK
------------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.CheckWork(Work)
-	local source = source
-	local Passport = vRP.Passport(source)
-	if Passport then
-		if vRP.GetWork(Passport) == Work then
-			return true
-		else
-			TriggerClientEvent("Notify",source,"amarelo","Você precisa ter a sua <b>Carteira de Trabalho</b> assinada no emprego de <b>"..ClassWork(Work).."</b> para conseguir trabalhar aqui.","Atenção",5000)
-		end
-
-		return false
-	end
-end
------------------------------------------------------------------------------------------------------------------------------------------
 -- PAYMENT
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Hensa.Payment(Service)
