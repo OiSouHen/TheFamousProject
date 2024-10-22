b4a45d3e89b6e93c320a8a170ae79883a12edeae
Lucas Hen <oisouhen@gmail.com>
2024-10-22 12:39:57 -0300

    Update core.lua

--- Base/resources/[scripts]/party/server/core.lua
+++ Base/resources/[scripts]/party/server/core.lua
@@ -1,18 +1,18 @@
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VRP
 -----------------------------------------------------------------------------------------------------------------------------------------
-local Tunnel = module("vrp", "lib/Tunnel")
-local Proxy = module("vrp", "lib/Proxy")
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CONNECTION
 -----------------------------------------------------------------------------------------------------------------------------------------
 Hensa = {}
-Tunnel.bindInterface("party", Hensa)
+Tunnel.bindInterface("party",Hensa)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
-local Amounts = 0
+local AmountRooms = 0
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CONFIG
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -28,19 +28,16 @@ function Hensa.GetRooms()
 	local Passport = vRP.Passport(source)
 	if Passport then
 		local Room = {}
-		for Index, v in pairs(Config["Room"]) do
-			if CountTable(v["Users"]) == 0 then
-				Room[#Room + 1] = nil
-				Config["Room"][#Room + 1] = nil
-			else
-				Room[#Room + 1] = {
-					["Identity"] = v["Identity"],
-					["Id"] = Index,
-					["Name"] = v["Name"],
-					["Password"] = v["Password"] or false,
-					["Users"] = CountTable(v["Users"])
-				}
-			end
+		for Index,v in pairs(Config["Room"]) do
+			Room[#Room + 1] = {
+				["Value"] = "",
+				["Id"] = v["Id"],
+				["Created"] = Index,
+				["Name"] = v["Name"],
+				["Identity"] = v["Identity"],
+				["Password"] = v["Password"] or false,
+				["Users"] = CountTable(v["Users"])
+			}
 		end
 
 		return {
@@ -52,37 +49,40 @@ end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- GETMEMBERS
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.GetMembers(Number)
+function Hensa.GetMembers(Room)
 	local source = source
+	local Room = parseInt(Room)
 	local Passport = vRP.Passport(source)
-	if Passport and Config["Room"][Number] and Config["Room"][Number]["Users"] then
-		local Room = {}
-		for OtherPassport, v in pairs(Config["Room"][Number]["Users"]) do
-			Room[#Room + 1] = {
+	if Passport and Config["Room"][Room] and Config["Room"][Room]["Users"] then
+		local Table = {}
+		for OtherPassport,v in pairs(Config["Room"][Room]["Users"]) do
+			Table[#Table + 1] = {
 				["Passport"] = OtherPassport,
 				["Name"] = vRP.FullName(OtherPassport)
 			}
 		end
+
 		return {
-			["Members"] = Room,
-			["Created"] = Config["Room"][Number]["Identity"],
-			["Identity"] = Config["Room"][Number]["Identity"],
-			["Name"] = Config["Room"][Number]["Name"],
-			["Users"] = CountTable(Config["Room"][Number]["Users"])
+			["Id"] = Room,
+			["Members"] = Table,
+			["Created"] = Config["Room"][Room]["Created"],
+			["Identity"] = Config["Room"][Room]["Identity"],
+			["Name"] = Config["Room"][Room]["Name"],
+			["Users"] = CountTable(Config["Room"][Room]["Users"])
 		}
 	end
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CREATEROOM
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.CreateRoom(Name, Password)
+function Hensa.CreateRoom(Name,Password)
 	local source = source
 	local Passport = vRP.Passport(source)
-
 	if Passport and not Config["Users"][Passport] then
-		Amounts = Amounts + 1
+		AmountRooms = AmountRooms + 1
 
-		Config["Room"][Amounts] = {
+		Config["Room"][AmountRooms] = {
+			["Id"] = AmountRooms,
 			["Created"] = Passport,
 			["Identity"] = vRP.FullName(Passport),
 			["Name"] = Name,
@@ -91,17 +91,18 @@ function Hensa.CreateRoom(Name, Password)
 				[Passport] = source
 			}
 		}
-		Config["Users"][Passport] = Amounts
 
-		vRP.Create(source, Passport, tostring(Amounts))
+		Config["Users"][Passport] = AmountRooms
 
 		return {
+			["group"] = AmountRooms,
 			["room"] = {
-				["Identity"] = Config["Room"][Amounts]["Identity"],
-				["Id"] = Amounts,
+				["Id"] = AmountRooms,
+				["Created"] = Passport,
+				["Identity"] = Config["Room"][AmountRooms]["Identity"],
 				["Name"] = Name,
-				["Password"] = Password or false,
-				["Users"] = CountTable(Config["Room"][Amounts]["Users"])
+				["Password"] = Password,
+				["Users"] = CountTable(Config["Room"][AmountRooms]["Users"])
 			}
 		}
 	end
@@ -109,102 +110,160 @@ function Hensa.CreateRoom(Name, Password)
 	return false
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
--- CREATEROOM
------------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.EnterRoom(Number, Password)
-	local source = source
-	local Passport = vRP.Passport(source)
-
-	if Config["Room"][Number] then
-		if Config["Users"][Passport] then
-			if Number ~= Config["Users"][Passport] then
-				if Config["Users"][Passport] then
-					Hensa.LeaveRoom(Config["Users"][Passport])
-				end
-
-				Config["Users"][Passport] = Number
-				Config["Room"][Number]["Users"][Passport] = source
-			end
-		else
-			Config["Users"][Passport] = Number
-			Config["Room"][Number]["Users"][Passport] = source
-
-			TriggerClientEvent("party:ResetNui", source)
-		end
-	end
-
-	vRP.Add(source, Passport, tostring(Number))
-end
------------------------------------------------------------------------------------------------------------------------------------------
 -- LEAVEROOM
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.LeaveRoom(Room)
+function Hensa.LeaveRoom()
 	local source = source
 	local Passport = vRP.Passport(source)
+	if Passport and Config["Users"][Passport] then
+		local Room = Config["Users"][Passport]
+		if Config["Room"][Room] and Config["Room"][Room]["Users"] and Config["Room"][Room]["Users"][Passport] then
+			for _,Sources in pairs(Config["Room"][Room]["Users"]) do
+				async(function()
+					TriggerClientEvent("party:Dismiss",Sources,source)
+				end)
+			end
 
-	if not Room then
-		Room = Config["Users"][Passport]
-	end
-
-	if Config["Room"][Room] then
-		if Room then
 			Config["Users"][Passport] = nil
 			Config["Room"][Room]["Users"][Passport] = nil
 
-			if CountTable(Config["Room"][Room]["Users"]) == 0 then
+			TriggerClientEvent("party:Clear",source)
+
+			if Config["Room"][Room]["Created"] == Passport then
+				for OtherPassport,v in pairs(Config["Room"][Room]["Users"]) do
+					TriggerClientEvent("party:ResetNui",v)
+					Config["Users"][OtherPassport] = nil
+				end
+
+				Config["Room"][Room] = nil
+			elseif CountTable(Config["Room"][Room]["Users"]) <= 0 then
 				Config["Room"][Room] = nil
 			end
-		end
 
-		TriggerClientEvent("party:ResetNui", source)
+			return true
+		end
 	end
 
-	vRP.Rem(source, Passport, tostring(Room))
+	return false
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- KICKROOM
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.KickRoom(Kick, Passport)
+function Hensa.KickRoom(Room,OtherPassport)
 	local source = source
-	local MyPassport = vRP.Passport(source)
+	local Room = parseInt(Room)
+	local Passport = vRP.Passport(source)
+	local OtherSource = vRP.Source(OtherPassport)
+	if Passport and Config["Users"][OtherPassport] and Config["Room"][Room] and Config["Room"][Room]["Created"] == Passport and Config["Room"][Room]["Created"] ~= OtherPassport and Config["Room"][Room]["Users"] and Config["Room"][Room]["Users"][OtherPassport] then
+		if OtherSource then
+			TriggerClientEvent("party:Clear",OtherSource)
+			TriggerClientEvent("party:ResetNui",OtherSource)
 
-	if Passport ~= MyPassport then
-		if Config["Users"][Passport] then
-			local Room = Config["Users"][Passport]
-
-			Config["Room"][Room]["Users"][Passport] = nil
-			Config["Users"][Passport] = nil
-
-			TriggerClientEvent("party:ResetNui", vRP.Source(Passport))
-
-			vRP.Rem(source, Passport, tostring(Room))
-
-			return Hensa.GetRooms()
-		end
-	end
-end
------------------------------------------------------------------------------------------------------------------------------------------
--- DISCONNECT
------------------------------------------------------------------------------------------------------------------------------------------
-AddEventHandler("Disconnect", function(Passport)
-	if Config["Users"][Passport] then
-		local source = vRP.Source(Passport)
-
-		if not Room then
-			Room = Config["Users"][Passport]
-		end
-
-		if Config["Room"][Room] then
-			if Room then
-				Config["Users"][Passport] = nil
-				Config["Room"][Room]["Users"][Passport] = nil
-
-				if CountTable(Config["Room"][Room]["Users"]) == 0 then
-					Config["Room"][Room] = nil
-				end
+			for _,Sources in pairs(Config["Room"][Room]["Users"]) do
+				async(function()
+					TriggerClientEvent("party:Dismiss",Sources,OtherSource)
+				end)
 			end
 		end
 
-		vRP.Rem(source, Passport, tostring(Room))
+		Config["Users"][OtherPassport] = nil
+		Config["Room"][Room]["Users"][OtherPassport] = nil
+
+		return true
+	end
+
+	return false
+end
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- ENTERROOM
+-----------------------------------------------------------------------------------------------------------------------------------------
+function Hensa.EnterRoom(Room,Password)
+	local source = source
+	local Room = parseInt(Room)
+	local Passport = vRP.Passport(source)
+	if Passport and not Config["Users"][Passport] and Config["Room"][Room] and Config["Room"][Room]["Users"] and CountTable(Config["Room"][Room]["Users"]) <= 9 and not Config["Room"][Room]["Users"][Passport] then
+		if Config["Room"][Room]["Password"] and Config["Room"][Room]["Password"] ~= Password then
+			return false
+		end
+
+		Config["Users"][Passport] = Room
+		Config["Room"][Room]["Users"][Passport] = source
+
+		for Passports,Sources in pairs(Config["Room"][Room]["Users"]) do
+			TriggerClientEvent("party:Invite",source,Sources,vRP.LowerName(Passports))
+
+			async(function()
+				TriggerClientEvent("party:Invite",Sources,source,vRP.LowerName(Passport))
+			end)
+		end
+
+		return true
+	end
+
+	return false
+end
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- ROOM
+-----------------------------------------------------------------------------------------------------------------------------------------
+exports("Room",function(Passport,source,Radius)
+	local Members = {}
+	local Room = Config["Users"][Passport]
+
+	if Room and Config["Room"][Room] then
+		local Coords = vRP.GetEntityCoords(source)
+
+		for OtherPassport,Sources in pairs(Config["Room"][Room]["Users"]) do
+			local OtherCoords = vRP.GetEntityCoords(Sources)
+
+			if #(Coords - OtherCoords) <= Radius then
+				Members[#Members + 1] = {
+					["Passport"] = OtherPassport,
+					["Source"] = Sources
+				}
+			end
+		end
+	end
+
+	return Members,CountTable(Members)
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- DOESEXIST
+-----------------------------------------------------------------------------------------------------------------------------------------
+exports("DoesExist",function(Passport,Players)
+	local Return = false
+
+	if Players then
+		if Config["Users"][Passport] then
+			local source = vRP.Source(Passport)
+			local Members = exports["party"]:Room(Passport,source,25)
+			if CountTable(Members) >= Players then
+				Return = Members
+			end
+		end
+	else
+		Return = Config["Users"][Passport]
+	end
+
+	return Return
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- DISCONNECT
+-----------------------------------------------------------------------------------------------------------------------------------------
+AddEventHandler("Disconnect",function(Passport,source)
+	if Config["Users"][Passport] then
+		local Room = Config["Users"][Passport]
+
+		for _,Sources in pairs(Config["Room"][Room]["Users"]) do
+			async(function()
+				TriggerClientEvent("party:Dismiss",Sources,source)
+			end)
+		end
+
+		Config["Users"][Passport] = nil
+		Config["Room"][Room]["Users"][Passport] = nil
+
+		if CountTable(Config["Room"][Room]["Users"]) <= 0 then
+			Config["Room"][Room] = nil
+		end
 	end
 end)
