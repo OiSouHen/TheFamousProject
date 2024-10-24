20ee81d95c676faedfb61a6f567762ec6bb6c259
Lucas Hen <oisouhen@gmail.com>
2024-10-24 00:17:51 -0300

    Update core.lua

--- Base/resources/[scripts]/grime/server/core.lua
+++ Base/resources/[scripts]/grime/server/core.lua
@@ -36,44 +36,52 @@ end)
 function Hensa.Payment(Selected)
 	local source = source
 	local Passport = vRP.Passport(source)
-	if Passport and not Active[Passport] and vRP.TakeItem(Passport,Item) then
-		Active[Passport] = true
+	if Passport and not Active[Passport] then
+		if not vRPC.LastVehicle(source,Model) then
+			TriggerClientEvent("Notify",source,"Atenção","Precisa utilizar o veículo do <b>Go Postal</b>.","amarelo",5000)
 
-		local Coords = vRP.GetEntityCoords(source)
-		if not Selected or #(Coords - Locations[Selected]) > 2.5 then
-			exports["discord"]:Embed("Hackers","**Passaporte:** "..Passport.."\n**Função:** Payment do Grime",0xa3c846,source)
+			return false
 		end
 
-		local GainExperience = 3
-		local Amount = math.random(175,275)
-		local Experience,Level = vRP.GetExperience(Passport,"Grime")
-		local Valuation = Amount + Amount * (0.05 * Level)
+		if vRP.TakeItem(Passport,Item,1,true) then
+			Active[Passport] = true
 
-		if exports["inventory"]:Buffs("Dexterity",Passport) then
-			Valuation = Valuation + (Valuation * 0.1)
-		end
-
-		if vRP.UserPremium(Passport) then
-			local Bonification = 0.05
-			local Hierarchy = vRP.LevelPremium(source)
-
-			if Hierarchy == 1 then
-				Bonification = 0.100
-			elseif Hierarchy == 2 then
-				Bonification = 0.075
+			local Coords = vRP.GetEntityCoords(source)
+			if not Selected or #(Coords - Locations[Selected]) > 2.5 then
+				exports["discord"]:Embed("Hackers","**Passaporte:** "..Passport.."\n**Função:** Payment do Grime",0xa3c846,source)
 			end
 
-			GainExperience = GainExperience + 2
-			Valuation = Valuation + (Valuation * Bonification)
+			local GainExperience = 3
+			local Amount = math.random(175,275)
+			local Experience,Level = vRP.GetExperience(Passport,"Grime")
+			local Valuation = Amount + Amount * (0.05 * Level)
+
+			if exports["inventory"]:Buffs("Dexterity",Passport) then
+				Valuation = Valuation + (Valuation * 0.1)
+			end
+
+			if vRP.UserPremium(Passport) then
+				local Bonification = 0.05
+				local Hierarchy = vRP.LevelPremium(source)
+
+				if Hierarchy == 1 then
+					Bonification = 0.100
+				elseif Hierarchy == 2 then
+					Bonification = 0.075
+				end
+
+				GainExperience = GainExperience + 2
+				Valuation = Valuation + (Valuation * Bonification)
+			end
+
+			vRP.GenerateItem(Passport,"dollar",Valuation,true)
+			vRP.PutExperience(Passport,"Grime",GainExperience)
+			vRP.UpgradeStress(Passport,3)
+
+			Active[Passport] = nil
+
+			return true
 		end
-
-		vRP.GenerateItem(Passport,"dollar",Valuation,true)
-		vRP.PutExperience(Passport,"Grime",GainExperience)
-		vRP.UpgradeStress(Passport,3)
-
-		Active[Passport] = nil
-
-		return true
 	end
 
 	return false
