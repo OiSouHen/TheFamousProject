0693e79beb736bbc4f1bc1a223818ac654df40c0
Lucas Hen <oisouhen@gmail.com>
2024-10-23 23:37:33 -0300

    Update core.lua

--- Base/resources/[scripts]/tackle/client/core.lua
+++ Base/resources/[scripts]/tackle/client/core.lua
@@ -8,18 +8,18 @@ CreateThread(function()
 		if not IsPedInAnyVehicle(Ped) and IsPedJumping(Ped) then
 			TimeDistance = 1
 
-			if IsControlJustReleased(1, 51) then
+			if IsControlJustReleased(1,51) then
 				local Tackle = {}
 				local Coords = GetEntityForwardVector(Ped)
 
-				SetPedToRagdollWithFall(Ped, 2500, 1500, 0, Coords, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+				SetPedToRagdollWithFall(Ped,2500,1500,0,Coords,1.0,0.0,0.0,0.0,0.0,0.0,0.0)
 
 				while IsPedRagdoll(Ped) do
-					for _, v in ipairs(GetPlayers()) do
+					for _,v in ipairs(GetPlayers()) do
 						if not Tackle[v] then
 							Tackle[v] = true
-							TriggerEvent("inventory:Cancel")
-							TriggerServerEvent("tackle:Update", GetPlayerServerId(v), Coords)
+							TriggerServerEvent("inventory:Cancel")
+							TriggerServerEvent("tackle:Update",GetPlayerServerId(v),Coords)
 						end
 					end
 
@@ -35,9 +35,9 @@ end)
 -- TACKLE:PLAYER
 -----------------------------------------------------------------------------------------------------------------------------------------
 RegisterNetEvent("tackle:Player")
-AddEventHandler("tackle:Player", function(Coords)
-	SetPedToRagdollWithFall(PlayerPedId(), 5000, 5000, 0, Coords["x"], Coords["y"], Coords["z"], 10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-	TriggerEvent("inventory:Cancel")
+AddEventHandler("tackle:Player",function(Coords)
+	SetPedToRagdollWithFall(PlayerPedId(),5000,5000,0,Coords["x"],Coords["y"],Coords["z"],10.0,0.0,0.0,0.0,0.0,0.0,0.0)
+	TriggerServerEvent("inventory:Cancel")
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- GETPLAYERS
@@ -46,9 +46,9 @@ function GetPlayers()
 	local Players = {}
 	local Ped = PlayerPedId()
 
-	for _, v in ipairs(GetActivePlayers()) do
+	for _,v in ipairs(GetActivePlayers()) do
 		local OtherPed = GetPlayerPed(v)
-		if IsEntityTouchingEntity(Ped, OtherPed) and not IsPedInAnyVehicle(OtherPed) and not IsEntityPlayingAnim(OtherPed, "amb@world_human_sunbathe@female@back@idle_a", "idle_a", 3) then
+		if IsEntityTouchingEntity(Ped,OtherPed) and not IsPedInAnyVehicle(OtherPed) and not IsEntityPlayingAnim(OtherPed,"amb@world_human_sunbathe@female@back@idle_a","idle_a",3) then
 			Players[#Players + 1] = v
 		end
 	end
