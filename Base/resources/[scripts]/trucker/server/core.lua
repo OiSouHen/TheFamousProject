9f24f603f1122a50e6cf50b413ec92a6291168a5
Lucas Hen <oisouhen@gmail.com>
2024-10-24 00:18:05 -0300

    Update core.lua

--- Base/resources/[scripts]/trucker/server/core.lua
+++ Base/resources/[scripts]/trucker/server/core.lua
@@ -13,91 +13,64 @@ Tunnel.bindInterface("trucker",Hensa)
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
-local Delay = {}
 local Active = {}
 -----------------------------------------------------------------------------------------------------------------------------------------
--- CHECKDELAY
+-- DROPS
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.CheckDelay()
-	local source = source
-	local Passport = vRP.Passport(source)
-	if Passport then
-		if Delay[Passport] then
-			if vRP.UserPremium(Passport) then
-				if Delay[Passport] >= 2 then
-					TriggerClientEvent("Notify",source,"amarelo","Você atingiu o limite diário.","Atenção",5000)
-					return true
-				end
-			else
-				TriggerClientEvent("Notify",source,"amarelo","Você atingiu o limite diário.","Atenção",5000)
-				return true
-			end
-		end
-
-		return false
-	end
-end
+local Drops = {
+	{ ["Item"] = "plastic", ["Chance"] = 75, ["Min"] = 225, ["Max"] = 275, ["Addition"] = 0.050 },
+	{ ["Item"] = "glass", ["Chance"] = 75, ["Min"] = 225, ["Max"] = 275, ["Addition"] = 0.050 },
+	{ ["Item"] = "rubber", ["Chance"] = 75, ["Min"] = 225, ["Max"] = 275, ["Addition"] = 0.050 },
+	{ ["Item"] = "aluminum", ["Chance"] = 25, ["Min"] = 175, ["Max"] = 200, ["Addition"] = 0.025 },
+	{ ["Item"] = "copper", ["Chance"] = 25, ["Min"] = 175, ["Max"] = 200, ["Addition"] = 0.025 }
+}
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- PAYMENT
 -----------------------------------------------------------------------------------------------------------------------------------------
-function Hensa.Payment(Service)
+function Hensa.Payment()
 	local source = source
 	local Passport = vRP.Passport(source)
 	if Passport and not Active[Passport] then
 		Active[Passport] = true
 
-		if Delay[Passport] == nil then
-			Delay[Passport] = 0
-		end
-
 		local Coords = vRP.GetEntityCoords(source)
 		if not vRPC.LastVehicle(source,"packer") or #(Coords - vec3(1256.59,-3239.63,5.17)) > 25 then
-			exports["discord"]:Embed("Payments","**Passaporte:** "..Passport.."\n**Função:** Payment do Trucker",0xa3c846)
+			exports["discord"]:Embed("Hackers","**Passaporte:** "..Passport.."\n**Função:** Payment do Trucker",0xa3c846,source)
 		end
 
-		local Experience = vRP.GetExperience(Passport,"Trucker")
-		local Level = ClassCategory(Experience)
-		local Valuation = math.random(1825,2525)
+		local GainExperience = 15
+		local Result = RandPercentage(Drops)
+		local Experience,Level = vRP.GetExperience(Passport,"Trucker")
+		local Valuation = Result["Valuation"] + Result["Valuation"] * (Result["Addition"] * Level)
 
-		if Level == 2 then
-			Valuation = Valuation + 150
-		elseif Level == 3 then
-			Valuation = Valuation + 250
-		elseif Level == 4 then
-			Valuation = Valuation + 350
-		elseif Level == 5 then
-			Valuation = Valuation + 450
-		elseif Level == 6 then
-			Valuation = Valuation + 550
-		elseif Level == 7 then
-			Valuation = Valuation + 650
-		elseif Level == 8 then
-			Valuation = Valuation + 750
-		elseif Level == 9 then
-			Valuation = Valuation + 850
-		elseif Level == 10 then
-			Valuation = Valuation + 950
-		end
-
-		local Buffs = exports["inventory"]:Buffs("Dexterity",Passport)
-		if Buffs and Buffs > os.time() then
+		if exports["inventory"]:Buffs("Dexterity",Passport) then
 			Valuation = Valuation + (Valuation * 0.1)
 		end
 
-		vRP.PaymentService(Passport,Valuation,vRP.Identity(Passport)["Mode"],true)
-		vRP.PutExperience(Passport,"Trucker",1)
+		if vRP.UserPremium(Passport) then
+			local Bonification = 0.05
+			local Hierarchy = vRP.LevelPremium(source)
 
-		if Service == "Vehicles" then
-			vRP.GenerateItem(Passport,"lockpick",math.random(3),true)
-		elseif Service == "Diesel" then
-			vRP.GenerateItem(Passport,"oilgallon",math.random(3),true)
-		elseif Service == "Fuel" then
-			vRP.GenerateItem(Passport,"WEAPON_PETROLCAN_AMMO",math.random(150,300),true)
-		elseif Service == "Wood" then
-			vRP.GenerateItem(Passport,"woodlog",math.random(5,10),true)
+			if Hierarchy == 1 then
+				Bonification = 0.100
+			elseif Hierarchy == 2 then
+				Bonification = 0.075
+			end
+
+			GainExperience = GainExperience + 10
+			Valuation = Valuation + (Valuation * Bonification)
 		end
 
-		Delay[Passport] = Delay[Passport] + 1
+		if not vRP.MaxItens(Passport,Result["Item"],Valuation) and vRP.CheckWeight(Passport,Result["Item"],Valuation) then
+			vRP.GenerateItem(Passport,Result["Item"],Valuation,true)
+		else
+			TriggerClientEvent("Notify",source,"Mochila Sobrecarregada","Sua recompensa caiu no chão.","roxo",5000)
+			exports["inventory"]:Drops(Passport,source,Result["Item"],Valuation)
+		end
+
+		vRP.PutExperience(Passport,"Trucker",GainExperience)
+		vRP.UpgradeStress(Passport,10)
+
 		Active[Passport] = nil
 	end
 end
